6ab2f233289d6376a0d7036d98c796acb009eb7b
Touhidul Sadeek <touhid.bd@live.com>
2023-04-14 16:19:55 +0600

    Initial commit

---
+++ src/components/Logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+    return (
+        <motion.div className='flex items-center justify-between mt-2'>
+            <MotionLink
+                href="/"
+                className='w-16 h-16 flex items-center justify-center rounded-full bg-dark text-light font-bold'
+                whileHover={{
+                    backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+                    transition: {
+                        duration: 1,
+                        repeat: Infinity
+                    }
+                }}
+            >TCBD</MotionLink>
+        </motion.div >
+    )
+}
+
+export default Logo
