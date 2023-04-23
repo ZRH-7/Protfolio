75eec8f56bc95fd015c5ec4801f82976cc3feabe
Touhidul Sadeek <touhid.bd@live.com>
2023-04-23 08:20:09 +0600

    Added about page

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -7,7 +7,7 @@ const HireMe = () => {
         <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
             <div className='w-48 h-48 flex items-center justify-center relative'>
                 <DevText className={"fill-dark animate-spin-slow"} />
-                <Link className='absolute left-[50%] translate-x-[-50%] bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full flex items-center justify-center font-semibold hover:bg-light hover:text-dark ' href={'mailto:touhidul.sadeek@gmail.com'}>Hire Me</Link>
+                <Link className='absolute transition-all left-[50%] translate-x-[-50%] bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full flex items-center justify-center font-semibold hover:bg-light hover:text-dark ' href={'mailto:touhidul.sadeek@gmail.com'}>Hire Me</Link>
             </div>
         </div>
     )
