b87cbe954015bc7da360f59d288933db53a16f09
Touhidul Sadeek <touhid.bd@live.com>
2023-05-10 13:11:52 +0600

    add experience section on about page

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,15 +1,87 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { motion, useScroll } from 'framer-motion'
+import { LiIcon } from './LiIcon'
+
+const Details = ({ position, company, companyLink, time, address, work }) => {
+    const ref = useRef(null);
+    return <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-center'>
+        <LiIcon reference={ref} />
+        <motion.div
+            initial={{ y: 50 }}
+            whileInView={{ y: 0 }}
+            transition={{ duration: 0.5, type: "spring" }}
+        >
+            <h3 className='capitalize font-bold text-2xl'>{position}
+                &nbsp;
+                <a className='text-primary capitalize' target='_blank' href={companyLink}>@{company}</a>
+            </h3>
+            <span className='capitalize font-medium text-dark/75'>
+                {time} | {address}
+            </span>
+            <p className='font-medium w-full'>{work}</p>
+        </motion.div>
+    </li>
+}
 
 export const Experience = () => {
+
+    const ref = useRef(null);
+    const { scrollYProgress } = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
+
     return (
         <>
             <div className='my-64'>
-                <h2 className='font-bold text-8xl w-full text-center'>Experience</h2>
-                <div className='w-[75%] mx-auto relative'>
-                    <ul>
-                        <li>
-
-                        </li>
+                <h2 className='font-bold text-8xl w-full text-center mb-32'>Experience</h2>
+                <div ref={ref} className='w-[75%] mx-auto relative'>
+                    <motion.div
+                        className='absolute left-9 top-0 w-[4px] h-full bg-dark origin-top'
+                        style={{ scaleY: scrollYProgress }}
+                    />
+                    <ul className='w-full flex flex-col items-start justify-between ml-4'>
+                        <Details
+                            position='Software Engineer'
+                            company='Google'
+                            companyLink='http://google.com'
+                            time='2022-Present'
+                            address='Mountain View, CA'
+                            work="Worked on a team responsible for developing new features for Google's search engine, including improving the accuracy and relevance of search results and developing new tools for data analysis and visualization."
+                        /><Details
+                            position='Intern'
+                            company='Facebook'
+                            companyLink='http://facebook.com'
+                            time='Summer 2021'
+                            address='Menlo Park, CA.'
+                            work='Worked on a team responsible for developing a new mobile app feature that allowed users to create and share short-form video content, including designing and implementing a new user interface and developing the backend infrastructure to support the feature.'
+                        />
+                        <Details
+                            position='Software Developer'
+                            company='Amazon'
+                            companyLink='http://amazon.com'
+                            time='2020-2021'
+                            address='Seattle, WA.'
+                            work="Worked on a team responsible for developing Amazon's mobile app, including implementing new features such as product recommendations and user reviews, and optimizing the app's performance and reliability."
+                        />
+                        <Details
+                            position='Software Developer Intern'
+                            company='Microsoft'
+                            companyLink='http://microsoft.com'
+                            time='Summer 2019'
+                            address='Redmond, WA.'
+                            work="Worked on a team responsible for developing new features for Microsoft's Windows operating system, including implementing a new user interface for a system settings panel and optimizing the performance of a core system component."
+                        />
+                        <Details
+                            position='Teaching Assistant'
+                            company='MIT'
+                            companyLink='http://mit.com'
+                            time='Fall 2018'
+                            address='Massachusetts Ave, Cambridge, MA.'
+                            work='Assisted in teaching a course on computer programming, held office hours to help students with assignments, and graded exams and assignments.'
+                        />
                     </ul>
                 </div>
             </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,35 +1,8 @@
-import AnimatedText from "@/components/AnimatedText"
 import Layout from "@/components/Layout"
 import Head from "next/head"
-import React, { useEffect, useRef } from "react"
-import ProfilePic from "../../public/images/profile/developer-pic-2.jpg"
-import Image from "next/image"
-import { useInView, useMotionValue, useSpring } from "framer-motion"
 import { Skills } from "@/components/Skills"
 import { Experience } from "@/components/Experience"
-
-const AnimatedNumbers = ({ value }) => {
-    const ref = useRef(null);
-    const motionValue = useMotionValue(0);
-    const springValue = useSpring(motionValue, { duration: 3000 });
-    const isInView = useInView(ref, { once: true });
-
-    useEffect(() => {
-        if (isInView) {
-            motionValue.set(value);
-        }
-    }, [isInView, value, motionValue]);
-
-    useEffect(() => {
-        springValue.on("change", (latest) => {
-            if (ref.current && latest.toFixed(0) <= value) {
-                ref.current.textContent = latest.toFixed(0);
-            }
-        })
-    }, [springValue, value])
-
-    return <span ref={ref}></span>
-}
+import { About } from "@/components/About"
 
 const about = () => {
     return (
@@ -41,33 +14,7 @@ const about = () => {
 
             <main className='flex w-full flex-col items-center justify-center'>
                 <Layout className="pt-16">
-                    <AnimatedText text="Passion Fuels Purpose!" className='!text-8xl mb-16' />
-                    <div className="grid w-full grid-cols-8 gap-16">
-                        <div className="col-span-3 flex flex-col items-start justify-start my-auto">
-                            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">Biography</h2>
-                            <p className="font-medium">Hi, I'm <strong>CodeBucks</strong>, a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. With 4 years of experience in the field. I am always looking for new and innovative ways to bring my clients' visions to life.</p>
-                            <p className="font-medium my-6">I believe that design is about more than just making things look pretty – it's about solving problems and creating intuitive, enjoyable experiences for users.</p>
-                            <p className="font-medium">Whether I'm working on a website, mobile app, or other digital product, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.</p>
-                        </div>
-                        <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark  bg-light p-8">
-                            <div className="absolute top-0 -right-3 -z-10 h-[103%] w-[102%]  rounded-[2rem] rounded-br-3xl bg-dark"></div>
-                            <Image src={ProfilePic} alt="tCoderBD" className="w-full h-auto rounded-2xl" />
-                        </div>
-                        <div className="col-span-2 flex flex-col items-end justify-center gap-16">
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={40} />+</span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Satisfied Clients</h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={300} />+</span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Projects Completed</h2>
-                            </div>
-                            <div className="flex flex-col items-end justify-center">
-                                <span className="inline-block text-7xl font-bold"><AnimatedNumbers value={7} />+</span>
-                                <h2 className="text-xl font-medium capitalize text-dark/75">Years Of Experience</h2>
-                            </div>
-                        </div>
-                    </div>
+                    <About />
                     <Skills />
                     <Experience />
                 </Layout>
